b202fa3e3ebc157cf1293a8792b63195381bced6
Josh Gannon <JoshG@Johnsonpace.com>
2023-10-17 14:37:20 -0500

    Separated Input component out

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-
-  // import Repl from "./Repl.svelte";
+    import QueryInput from "./lib/QueryInput.svelte";
 
   let inner: Element;
   let query = `[:find (pull ?h [*])
@@ -15,34 +13,14 @@
     }
   };
 
-  let focusRef;
-
-  onMount(() => {
-    focusRef.focus();
-  });
-
-  let inputText :string;
-
-  $: console.log(inputText);
-
-  function createQuery(): void {
-    //parse inputText string into separate values
-    //build Dataloge query based on parsed values
-    //paste query into contents.md file and show in right panel
-  }
-
-  //create event listener for 'enter' to run createQuery()
 
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <main on:click={clickOutside}>
-  <div bind:this={inner} class="container">
-    <div class="input-container">
-      <p>Smart Query:</p>
-      <input type="text" bind:value={inputText} bind:this={focusRef}/>
-    </div>
-    <button on:click={createQuery}>Submit</button>
+  <div bind:this={inner} class="container" id="smart-query-container">
+    <QueryInput/>
+    <button>Submit</button>
   </div>
 </main>
 
@@ -60,7 +38,6 @@
   .container {
     position: fixed;
     top: 50px;
-    left: 0;
     background-color: azure;
     box-shadow: 0px 0.25rem 0.5rem 0px darkolivegreen;
     border-radius: 2px;
@@ -68,25 +45,6 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
-  }
-
-  .input-container {
-    padding: 0.5rem 0;
-    display: flex;
-    align-items: center;
-  }
-
-  p {
-    margin: 0;
-    padding-left: 0.5rem;
-  }
-
-  input {
-    font-size: 1.25rem;
-    border: none;
-    margin: 0 0.5rem;
-    width: 600px;
-    background-color: transparent;
   }
 
   button {
---
+++ src/lib/QueryInput.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import {inputText} from './store'
+    import {createQuery} from '../createQuery'
+
+  $: createQuery($inputText);
+</script>
+
+<div class="input-container">
+    <p>Smart Query:</p>
+    <input type="text" bind:value={$inputText}/>
+  </div>
+
+<style>
+    .input-container {
+    padding: 0.5rem 0;
+    display: flex;
+    align-items: center;
+  }
+
+  p {
+    margin: 0;
+    padding-left: 0.5rem;
+  }
+
+  input {
+    font-size: 1.25rem;
+    border: none;
+    margin: 0 0.5rem;
+    width: 600px;
+    background-color: transparent;
+  }
+</style>
